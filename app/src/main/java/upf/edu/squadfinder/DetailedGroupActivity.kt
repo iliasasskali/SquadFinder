6eb34cf099fa83e0be1e18d49e17d5b7cebafee6
Ilias Asskali <iliasasskali2010@gmail.com>
2021-03-08 17:30:53 +0100

    Detall grup

---
+++ app/src/main/java/upf/edu/squadfinder/DetailedGroupActivity.kt
@@ -0,0 +1,11 @@
+package upf.edu.squadfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailedGroupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detaills_grup_membre)
+    }
+}
