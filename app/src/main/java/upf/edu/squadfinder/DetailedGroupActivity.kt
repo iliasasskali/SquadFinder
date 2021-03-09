01a2e3cdb7e91c0c970174aa580eb10da8523020
Ilias Asskali <iliasasskali2010@gmail.com>
2021-03-09 23:39:34 +0100

    Petits canvis

--- app/src/main/java/upf/edu/squadfinder/DetailedGroupActivity.kt
+++ app/src/main/java/upf/edu/squadfinder/DetailedGroupActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class DetailedGroupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detaills_grup_membre)
+        setContentView(R.layout.activity_detalls_grup_membre)
     }
 }
