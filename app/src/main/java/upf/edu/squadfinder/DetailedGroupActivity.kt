152e5ceab477914fdf39970003589cd5a69cc450
Ilias Asskali <iliasasskali2010@gmail.com>
2021-03-10 23:10:31 +0100

    Canvis en detall grup

--- app/src/main/java/upf/edu/squadfinder/DetailedGroupActivity.kt
+++ app/src/main/java/upf/edu/squadfinder/DetailedGroupActivity.kt
@@ -4,9 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Button
 import android.widget.LinearLayout
-import android.widget.Toast
 import androidx.core.view.isVisible
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import upf.edu.squadfinder.ui.chat.ChatActivity
@@ -14,7 +12,7 @@ import upf.edu.squadfinder.ui.chat.ChatActivity
 class DetailedGroupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detalls_grup_membre)
+        setContentView(R.layout.activity_detall_grup)
 
         // showing the back button in action bar
         getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
