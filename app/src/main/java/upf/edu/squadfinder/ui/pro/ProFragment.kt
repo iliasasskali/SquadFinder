cffe77ea30ffa6c848437c1b30d542866bacd55f
Ilias Asskali <iliasasskali2010@gmail.com>
2021-03-12 00:34:58 +0100

    pro

--- app/src/main/java/upf/edu/squadfinder/ui/pro/ProFragment.kt
+++ app/src/main/java/upf/edu/squadfinder/ui/pro/ProFragment.kt
@@ -4,28 +4,53 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListAdapter
+import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.*
 import upf.edu.squadfinder.R
+import upf.edu.squadfinder.databinding.FragmentGrupsBinding
+import upf.edu.squadfinder.databinding.FragmentProBinding
+import upf.edu.squadfinder.ui.grups.GrupsRecyclerViewAdapter
 
 class ProFragment : Fragment() {
 
     private lateinit var proViewModel: ProViewModel
+    private lateinit var proAdvantages: ListView
+
+    private var _binding: FragmentProBinding? = null
+    private val binding get() = _binding!!
+
+    private val proAdvantagesStrings = arrayOf(
+            "Elimina la publicitat",
+            "Crea grups de forma ilimitada",
+            "Crea esdeveniments de forma ilimitada",
+            "Properament funcionalitats exclusives")
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        proViewModel =
-                ViewModelProvider(this).get(ProViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_pro, container, false)
-        val textView: TextView = root.findViewById(R.id.text_pro)
-        proViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        _binding = FragmentProBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.proAdvantages.apply {
+            binding.proAdvantages.adapter = ArrayAdapter(context, R.layout.llista_pro, proAdvantagesStrings)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
