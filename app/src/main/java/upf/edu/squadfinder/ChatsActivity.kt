ed7d60d393fa5903d09b4b8ce58e140e560b322a
Ilias Asskali <iliasasskali2010@gmail.com>
2021-03-14 17:15:29 +0100

    Tot acabat

--- app/src/main/java/upf/edu/squadfinder/ChatsActivity.kt
+++ app/src/main/java/upf/edu/squadfinder/ChatsActivity.kt
@@ -14,6 +14,7 @@ class ChatsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chats)
+        this.title = "Xats"
 
         // showing the back button in action bar
         getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
